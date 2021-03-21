08a4f8e672ad6effcd3a1b765d0215bd2f71a240
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-03-20 17:41:34 -0700

    organize code a bit, debug axis stuff

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,12 @@
 #include <iostream>
 
 using namespace vex;
+using namespace std;
+
+competition Competition;
+
+controller::axis turnAxis() {return Controller1.Axis1;}
+controller::axis driveAxis() {return Controller1.Axis2;}
 
 controller::button openClaw() {return Controller1.ButtonL1;}
 controller::button closeClaw() {return Controller1.ButtonL2;}
@@ -27,100 +33,115 @@ controller::button liftArm() {return Controller1.ButtonR1;}
 controller::button dropArm() {return Controller1.ButtonR2;}
 
 void turnRobot() {
-  if (Controller1.Axis1.position(percent) == 0) {
+  if (turnAxis().position(percent) == 0) {
     Drivetrain.setTurnVelocity(0, percent);
   }
 
-  if (Controller1.Axis1.position(percent) > 0) {
-    Drivetrain.turn(right);
+  if (turnAxis().position(percent) > 0) {
+    Drivetrain.turn(vex::right);
   } else {
-    Drivetrain.turn(left);
+    Drivetrain.turn(vex::left);
   }
 }
 
 void driveRobot() {
-
-  if (Controller1.Axis2.position(percent) == 0) {
+  cout << driveAxis().position() << endl;
+  if (driveAxis().position() == 0) {
     Drivetrain.stop();
   }
 
-  if (Controller1.Axis2.position(percent) > 0) {
-    Drivetrain.drive(forward);
+  if (driveAxis().position() > 0) {
+    Drivetrain.drive(vex::forward);
   } else {
-    Drivetrain.drive(reverse);
+    Drivetrain.drive(vex::reverse);
   }
 
 }
 
 void moveClaw() {
-  Brain.Screen.print("moev claw");
+  Brain.Screen.print("move claw");
+  cout << "move claw" << endl;
   Brain.Screen.print(openClaw().pressing());
+  cout << openClaw().pressing() << endl;
   Brain.Screen.print(closeClaw().pressing());
+  cout << closeClaw().pressing() << endl;
 
   if (openClaw().pressing()) {
-    ClawMotor.spin(forward);
+    ClawMotor.spin(vex::forward);
   } else if (closeClaw().pressing()) {
-    ClawMotor.spin(reverse);
+    ClawMotor.spin(vex::reverse);
   } else {
     ClawMotor.stop();
   }
 }
 
 void moveArm() {
-  Brain.Screen.print("moev arm");
+  Brain.Screen.print("move arm");
+  cout << "move arm" << endl;
   Brain.Screen.print(liftArm().pressing());
+  cout << liftArm().pressing() << endl;
   Brain.Screen.print(dropArm().pressing());
+  cout << dropArm().pressing() << endl;
+
   if (liftArm().pressing()) {
-    ArmMotor.spin(forward);
+    ArmMotor.spin(vex::forward);
   } else if (dropArm().pressing()) {
-    ArmMotor.spin(reverse);
+    ArmMotor.spin(vex::reverse);
   } else {
     ArmMotor.stop();
   }
 }
 
-
 bool isGreenCube() {
   Vision3.takeSnapshot(Vision3__SIG_3);
-
   bool isGreen = Vision3.objects[0].exists;
-
   return isGreen;
+}
+
+void teleopMode() {
+  turnAxis().changed(turnRobot);
+  driveAxis().changed(driveRobot);
+
+  openClaw().pressed(moveClaw);
+  openClaw().released(moveClaw);
+  closeClaw().pressed(moveClaw);
+  closeClaw().released(moveClaw);
+
+  liftArm().pressed(moveArm);
+  liftArm().released(moveArm);
+  dropArm().pressed(moveArm);
+  dropArm().released(moveArm);
+}
+
+void autoMode() {
+  // setDriveVelocity();
+
 }
 
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
 
+  Drivetrain.setDriveVelocity(50, percent);
+  ClawMotor.setVelocity(50, percent);
+  ArmMotor.setVelocity(50, percent);
+
+  Drivetrain.setStopping(brake);
+  ClawMotor.setStopping(hold);
+  ArmMotor.setStopping(hold);
+
   while (true) {
-    std::cout << isGreenCube() << std::endl;
+    cout << isGreenCube() << endl;
     Brain.Screen.print(isGreenCube());
-
-    Drivetrain.setDriveVelocity(50, percent);
-    ClawMotor.setVelocity(50, percent);
-    ArmMotor.setVelocity(50, percent);
-
-    Drivetrain.setStopping(brake);
-    ClawMotor.setStopping(hold);
-    ArmMotor.setStopping(hold);
-
-    Controller1.Axis1.changed(turnRobot);
-    Controller1.Axis2.changed(driveRobot);
-
-    openClaw().pressed(moveClaw);
-    openClaw().released(moveClaw);
-    closeClaw().pressed(moveClaw);
-    closeClaw().released(moveClaw);
-
-    liftArm().pressed(moveArm);
-    liftArm().released(moveArm);
-    dropArm().pressed(moveArm);
-    dropArm().released(moveArm);
-
   }
+
+  teleopMode();
+  autoMode();
+
+  // Competition.autonomous(autoMode);
+  // Competition.drivercontrol(teleopMode);
   
   return 0;
-
 }
 
 
